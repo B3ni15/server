5433282ead0c55f56db3a450675c4af3375cead4
B3ni <xbenitmx2020@gmail.com>
2024-08-25 20:27:51 +0200

    Update headless option in puppeteer.launch

--- routes/user/login.js
+++ routes/user/login.js
@@ -14,7 +14,7 @@ module.exports = async function (req, res) {
     (async () => {
       try {
         const browser = await puppeteer.launch({
-          headless: false,
+          headless: true,
           args: [
             '--no-sandbox',
             '--disable-setuid-sandbox'
