57ba8c099a0ea7fd94fbcecb8968964906ce9038
devbeni <ballobeni08@gmail.com>
2025-01-21 12:45:41 +0100

    fix: Add platform argument to Puppeteer launch options for improved compatibility

--- routes/user/login.js
+++ routes/user/login.js
@@ -18,7 +18,7 @@ module.exports = async function (req, res) {
     console.log('[INFO] Launching Puppeteer browser...');
     const browser = await puppeteer.launch({
       headless: true,
-      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      args: ['--no-sandbox', '--disable-setuid-sandbox', '--platform', 'linux'],
     });
 
     const page = await browser.newPage();
