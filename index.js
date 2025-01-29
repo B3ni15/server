0e1e3a60d7c759df742edbde324422c05e191522
B3ni <ballobeni08@gmail.com>
2025-01-29 20:30:40 +0100

    Remove unused dependencies and functions

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 const express = require('express');
 const axios = require('axios');
-const puppeteer = require('puppeteer');
-const moment = require('moment');
-const fs = require('fs');
-const path = require('path');
 const app = express();
 const port = 3000;
 
@@ -55,87 +51,6 @@ app.get('/api/user/evaluations', async (req, res) => {
     res.status(400).json({ error: error.message });
   }
 });
-
-async function processPuppeteer() {
-  try {
-    const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox'] });
-    const page = await browser.newPage();
-    await page.goto(
-      'https://idp.e-kreta.hu/Account/Login?ReturnUrl=/connect/authorize/callback?prompt=login&response_type=code&client_id=kreta-ellenorzo-student-mobile-ios'
-    );
-
-    await page.type('#UserName', process.env.TAJ);
-    await page.type('#Password', process.env.PASSWORD);
-
-    await new Promise((resolve) => setTimeout(resolve, 2000));
-    await page.click('#submit-btn');
-    await page.waitForNavigation();
-
-    const redirectedUrl = page.url();
-    const urlParams = new URLSearchParams(new URL(redirectedUrl).search);
-    const code = urlParams.get('code');
-
-    if (code) {
-      const accessToken = await getAccessToken(code);
-      console.log('Access Token:', accessToken);
-    }
-
-    await browser.close();
-  } catch (error) {
-    console.error('Error during Puppeteer process:', error);
-  }
-}
-
-async function getAccessToken(code) {
-  try {
-    const response = await axios.post(
-      'https://idp.e-kreta.hu/connect/token',
-      new URLSearchParams({
-        grant_type: 'authorization_code',
-        code: code,
-        client_id: 'kreta-ellenorzo-student-mobile-ios',
-        redirect_uri: 'https://mobil.e-kreta.hu/ellenorzo-student/prod/oauthredirect',
-        code_verifier: 'DSpuqj_HhDX4wzQIbtn8lr8NLE5wEi1iVLMtMK0jY6c',
-      }),
-      {
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      }
-    );
-    return response.data.access_token;
-  } catch (error) {
-    console.error('Error fetching access token:', error.message);
-    throw error;
-  }
-}
-
-setInterval(async () => {
-  await processPuppeteer();
-
-  const directory = path.join(__dirname, 'kepek');
-  fs.readdir(directory, (err, files) => {
-    if (err) {
-      console.error('Error reading directory:', err);
-      return;
-    }
-    files.forEach((file) => {
-      fs.unlink(path.join(directory, file), (err) => {
-        if (err) {
-          console.error('Error deleting file:', file, err);
-        } else {
-          console.log('File deleted:', file);
-        }
-      });
-    });
-  });
-}, 30 * 60 * 1000);
-
-function getCurrentMonday() {
-  return moment().startOf('isoWeek').format('YYYY-MM-DD');
-}
-
-function getCurrentFriday() {
-  return moment().endOf('isoWeek').format('YYYY-MM-DD');
-}
 
 console.log('Current Monday:', getCurrentMonday(), 'Current Friday:', getCurrentFriday());
 
